86d54c15b00e113b04de41ca9b35a9ffdc2d64c5
Devix <thanhvidev@gmail.com>
2022-05-24 21:22:10 +0700

    UpLoad 24/05/2022

--- TheLoai/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "login") or die("Kết nối CSDL thất bại");
-    mysqli_set_charset($conn, "utf8");
-?>
--- TheLoai/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root","","example") or die("Kết nối CSDL thất bại");
-    mysqli_set_charset( $conn ,"utf8");
-?>
